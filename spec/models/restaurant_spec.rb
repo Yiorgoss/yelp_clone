7b82077e6faf7ba331f42df0ae70b4b33dac7f79
yiorgoss <makridakisyiorgos@gmail.com>
2016-03-01 19:01:11 +0000

    added validation for review(1-5), for restaurant creationg(name>2, uniquness)

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 require 'spec_helper'
+
 describe Restaurant, type: :model do
   it {is_expected.to have_many :reviews }
 end
@@ -10,9 +11,15 @@ end
 
 
 describe Restaurant, :type => :model do
-it 'is not valid with a name of less than three characters' do
-  restaurant = Restaurant.new(name: 'kf')
-  expect(restaurant).to have(1).error_on(:name)
-  expect(restaurant).not_to be_valid
-end
+  it 'is not valid with a name of less than three characters' do
+    restaurant = Restaurant.new(name: 'kf')
+    expect(restaurant).to have(1).error_on(:name)
+    expect(restaurant).not_to be_valid
+  end
+
+  it 'does not accept restaurant unless unique' do
+    Restaurant.create(name: 'whatever')
+    restaurant = Restaurant.new(name: 'whatever')
+    expect(restaurant).to have(1).error_on(:name)
+  end
 end
