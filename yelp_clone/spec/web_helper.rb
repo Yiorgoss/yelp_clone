0a702a702597a0bcd3a0f6661de306f62e8cda16
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-03 17:08:42 +0000

    stars added

--- yelp_clone/spec/web_helper.rb
+++ yelp_clone/spec/web_helper.rb
@@ -20,3 +20,11 @@ def sign_up_other
   fill_in 'Password confirmation', with: 'password123'
   click_button 'Sign up'
 end
+
+def leave_review(thoughts, rating)
+  visit '/restaurants'
+  click_link 'Review KFC'
+  fill_in 'Thoughts', with: thoughts
+  select rating, from: 'Rating'
+  click_button 'Leave Review'
+end
